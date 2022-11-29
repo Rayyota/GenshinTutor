5115a0ca87d7e8e6f5a47008829db471d30438cc
Mariya <lapovama@gmail.com>
2022-11-29 06:57:14 +0600

    [+] Added services

--- src/main/java/com/genshintutor/demo/repo/CharacterRepository.java
+++ src/main/java/com/genshintutor/demo/repo/CharacterRepository.java
@@ -2,6 +2,14 @@ package com.genshintutor.demo.repo;
 
 import com.genshintutor.demo.models.Character;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CharacterRepository extends CrudRepository<Character, Long> {
+
 }
+
+//public interface CharacterRepository extends PagingAndSortingRepository<Character, Long> {
+//
+//}
